0a3eadaa08014d60ba75156d5c8bf8687076b0b2
Neon <toyzz@live.com>
2018-03-21 18:49:48 -0700

    Full command line... Set up...

--- main.go
+++ main.go
@@ -14,15 +14,45 @@ import (
 )
 
 var (
-	runOnce = flag.Bool("once", false, "Only run the program once")
+	options = make([]wallhaven.Option, 0)
 )
 
 func main() {
+	var (
+		// cats specifies the enabled wallpaper categories.
+		cats wallhaven.Categories
+		// purity specifies the enabled purity modes.
+		purity wallhaven.Purity
+		// res specifies the enabled screen resolutions.
+		res wallhaven.Resolutions
+		// ratios specifies the enabled aspect rations.
+		ratios wallhaven.Ratios
+		// Run Once
+		runOnce bool
+	)
+
+	flag.Var(&cats, "cats", "Wallpaper categories (general,anime,people)")
+	flag.Var(&purity, "purity", "Purity modes (SFW,sketchy)")
+	flag.Var(&res, "res", "Screen resolutions (1024x768,1280x800,1366x768,1280x960,1440x900,1600x900,1280x1024,1600x1200,1680x1050,1920x1080,1920x1200,2560x1440,2560x1600,3840x1080,5760x1080,3840x2160)")
+	flag.Var(&ratios, "ratios", "Aspect ratios (4x3,5x4,16x9,16x10,21x9,32x9,48x9)")
+	flag.BoolVar(&runOnce, "once", false, "Only run the program once")
 	flag.Parse()
 
-	rand.Seed(time.Now().Unix())
+	if cats != 0 {
+		options = append(options, cats)
+	}
+	if purity != 0 {
+		options = append(options, purity)
+	}
+	if res != 0 {
+		options = append(options, res)
+	}
+	if ratios != 0 {
+		options = append(options, ratios)
+	}
 
-	if *runOnce {
+	rand.Seed(time.Now().Unix())
+	if runOnce {
 		changeWallpaper()
 		return
 	}
@@ -36,8 +66,10 @@ func main() {
 func changeWallpaper() {
 	var page wallhaven.Page
 	page.Set(strconv.Itoa(random(1, 3))) // between 1 or 2...
+	options = append(options, page)
+	options = append(options, wallhaven.SortRandom)
 
-	havenIDs, _ := wallhaven.Search("", wallhaven.CatAnime, wallhaven.Ratio16x9, wallhaven.SortRandom, wallhaven.PuritySketchy, page)
+	havenIDs, _ := wallhaven.Search("", options...)
 
 	background, err := wallpaper.Get()
 
