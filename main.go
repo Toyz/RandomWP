d8bb85fe050ef1a597c55e073c148deb4a00cc8c
Neon <toyzz@live.com>
2018-03-21 19:08:03 -0700

    added a way to show notifications if you want too

--- main.go
+++ main.go
@@ -15,27 +15,30 @@ import (
 
 var (
 	options = make([]wallhaven.Option, 0)
+	// cats specifies the enabled wallpaper categories.
+	cats wallhaven.Categories
+	// purity specifies the enabled purity modes.
+	purity wallhaven.Purity
+	// res specifies the enabled screen resolutions.
+	res wallhaven.Resolutions
+	// ratios specifies the enabled aspect rations.
+	ratios wallhaven.Ratios
+	// Run Once
+	runOnce bool
+	// Delay for loop between changes
+	delay int64
+	// Send notifcations (default false)
+	notify bool
 )
 
 func main() {
-	var (
-		// cats specifies the enabled wallpaper categories.
-		cats wallhaven.Categories
-		// purity specifies the enabled purity modes.
-		purity wallhaven.Purity
-		// res specifies the enabled screen resolutions.
-		res wallhaven.Resolutions
-		// ratios specifies the enabled aspect rations.
-		ratios wallhaven.Ratios
-		// Run Once
-		runOnce bool
-	)
-
 	flag.Var(&cats, "cats", "Wallpaper categories (general,anime,people)")
 	flag.Var(&purity, "purity", "Purity modes (SFW,sketchy)")
 	flag.Var(&res, "res", "Screen resolutions (1024x768,1280x800,1366x768,1280x960,1440x900,1600x900,1280x1024,1600x1200,1680x1050,1920x1080,1920x1200,2560x1440,2560x1600,3840x1080,5760x1080,3840x2160)")
 	flag.Var(&ratios, "ratios", "Aspect ratios (4x3,5x4,16x9,16x10,21x9,32x9,48x9)")
+	flag.Int64Var(&delay, "delay", 30, "Delay between background changes (in seconds)")
 	flag.BoolVar(&runOnce, "once", false, "Only run the program once")
+	flag.BoolVar(&notify, "notify", false, "Show notification when wallpaper changes")
 	flag.Parse()
 
 	if cats != 0 {
@@ -59,7 +62,7 @@ func main() {
 
 	for {
 		changeWallpaper()
-		time.Sleep(30 * time.Second)
+		time.Sleep(time.Duration(delay) * time.Second)
 	}
 }
 
@@ -82,8 +85,10 @@ func changeWallpaper() {
 	fmt.Printf("New Wallpaper: %s\n", file)
 	wallpaper.SetFromFile(file)
 
-	err = beeep.Notify("Changed Wallpaper", "Your wallpaper has been changed", "assets/information.png")
-	isError(err)
+	if notify {
+		err = beeep.Notify("Changed Wallpaper", "Your wallpaper has been changed", "assets/information.png")
+		isError(err)
+	}
 
 	deleteFile(file)
 }
