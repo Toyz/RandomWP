2495af63fc52efbb491f020e49f610bbdfc20554
Neon <toyzz@live.com>
2018-03-21 18:36:03 -0700

    Added run once...  Working on a settings file now

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -9,31 +10,50 @@ import (
 
 	"github.com/Toyz/RandomWP/wallhaven"
 	"github.com/Toyz/RandomWP/wallpaper"
+	"github.com/gen2brain/beeep"
+)
+
+var (
+	runOnce = flag.Bool("once", false, "Only run the program once")
 )
 
 func main() {
+	flag.Parse()
+
 	rand.Seed(time.Now().Unix())
-	var page wallhaven.Page
+
+	if *runOnce {
+		changeWallpaper()
+		return
+	}
 
 	for {
-		page.Set(strconv.Itoa(random(1, 3))) // between 1 or 2...
-
-		havenIDs, _ := wallhaven.Search("anime", wallhaven.CatAnime, wallhaven.Ratio16x9, wallhaven.SortRandom, wallhaven.PuritySketchy, page)
-
-		background, err := wallpaper.Get()
-
-		if isError(err) {
-			continue
-		}
-
-		fmt.Printf("Current wallpaper: %s\n", background)
-		file, _ := havenIDs[rand.Intn(len(havenIDs))].Download(os.TempDir())
-		fmt.Printf("New Wallpaper: %s\n", file)
-		wallpaper.SetFromFile(file)
-
-		deleteFile(file)
+		changeWallpaper()
 		time.Sleep(30 * time.Second)
 	}
+}
+
+func changeWallpaper() {
+	var page wallhaven.Page
+	page.Set(strconv.Itoa(random(1, 3))) // between 1 or 2...
+
+	havenIDs, _ := wallhaven.Search("", wallhaven.CatAnime, wallhaven.Ratio16x9, wallhaven.SortRandom, wallhaven.PuritySketchy, page)
+
+	background, err := wallpaper.Get()
+
+	if isError(err) {
+		return
+	}
+
+	fmt.Printf("Current wallpaper: %s\n", background)
+	file, _ := havenIDs[rand.Intn(len(havenIDs))].Download(os.TempDir())
+	fmt.Printf("New Wallpaper: %s\n", file)
+	wallpaper.SetFromFile(file)
+
+	err = beeep.Notify("Changed Wallpaper", "Your wallpaper has been changed", "assets/information.png")
+	isError(err)
+
+	deleteFile(file)
 }
 
 func deleteFile(path string) {
